5edf2cba3cc761116a32dbedd511010bf1fe8c43
pootking221 <pootking221@gmail.com>
2025-01-14 10:37:24 +0700

    project version 2

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+function remove (){
+    document.getElementById("text").innerText = "Hello wellcome to Githup pootking221@gmail.com"
+    setInterval(() => {
+        document.getElementById("text").innerText = "Thank you!"
+    }, 3000);
+    
+}
